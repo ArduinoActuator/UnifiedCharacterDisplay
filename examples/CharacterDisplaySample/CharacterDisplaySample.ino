8bbe2aa184d8083b29f7e16efabd9bae41097627
ぶり <houtbrion@gmail.com>
2024-07-28 09:10:10 +0900

    first version.

---
+++ examples/CharacterDisplaySample/CharacterDisplaySample.ino
@@ -0,0 +1,255 @@
+
+#include "UnifiedCharacterDisplay.h"
+
+//#define USE_GROVE
+#define USE_LIQUID_CRYSTAL
+#define USE_ACM1602NI
+
+#ifdef USE_GROVE
+rgb_lcd grove_lcd;
+UnifiedLCD lcd(&(grove_lcd), GROVE_LCD_RGB_BACKLIGHT, Wire);
+const int colorR = 255;
+const int colorG = 0;
+const int colorB = 0;
+#endif /* USE_GROVE */
+
+#ifdef USE_LIQUID_CRYSTAL
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
+const int rs = 12, en = 10, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal liquid_crystal_h(rs, en, d4, d5, d6, d7);
+UnifiedLCD liquid_crystal(&liquid_crystal_h, LIQUID_CRYSTAL);
+#endif /* USE_LIQUID_CRYSTAL */
+
+#ifdef USE_ACM1602NI
+ACM1602NI liquid_crystal_i2c_h;  // set the LCD address to 0x27 for a 16 chars and 2 line display
+UnifiedLCD liquid_crystal_i2c(&liquid_crystal_i2c_h, LIQUID_CRYSTAL_I2C);
+#endif /* USE_ACM1602NI */
+
+void clearDisplay(void){
+#ifdef USE_GROVE
+  lcd.setCursor(0, 0);
+#endif /* USE_GROVE */
+
+#ifdef USE_LIQUID_CRYSTAL
+  liquid_crystal.setCursor(0, 0);
+#endif /* USE_LIQUID_CRYSTAL */
+
+#ifdef USE_ACM1602NI
+  liquid_crystal_i2c.setCursor(0, 0);
+#endif /* USE_ACM1602NI */
+
+#ifdef USE_GROVE
+  lcd.setBacklightRGB(255, 255, 255);
+#endif /* USE_GROVE */
+
+#ifdef USE_GROVE
+  lcd.setCursor(0, 1);
+#endif /* USE_GROVE */
+
+#ifdef USE_LIQUID_CRYSTAL
+  liquid_crystal.setCursor(0, 1);
+#endif /* USE_LIQUID_CRYSTAL */
+
+#ifdef USE_ACM1602NI
+  liquid_crystal_i2c.setCursor(0, 1);
+#endif /* USE_ACM1602NI */
+
+#ifdef USE_GROVE
+  lcd.clear();
+#endif /* USE_GROVE */
+
+#ifdef USE_LIQUID_CRYSTAL
+  liquid_crystal.clear();
+#endif /* USE_LIQUID_CRYSTAL */
+
+#ifdef USE_ACM1602NI
+  liquid_crystal_i2c.clear();
+#endif /* USE_ACM1602NI */
+
+}
+
+void helloWorld(void){
+  Serial.println("Hello World : start");
+
+#ifdef USE_GROVE
+  lcd.setCursor(0, 0);
+#endif /* USE_GROVE */
+
+#ifdef USE_LIQUID_CRYSTAL
+  liquid_crystal.setCursor(0, 0);
+#endif /* USE_LIQUID_CRYSTAL */
+
+#ifdef USE_ACM1602NI
+  liquid_crystal_i2c.setCursor(0, 0);
+#endif /* USE_ACM1602NI */
+
+#ifdef USE_GROVE
+  lcd.setBacklightRGB(colorR, colorG, colorB);
+#endif /* USE_GROVE */
+
+  // Print a message to the LCD.
+
+#ifdef USE_GROVE
+  lcd.print("hello, world!");
+#endif /* USE_GROVE */
+
+#ifdef USE_LIQUID_CRYSTAL
+  liquid_crystal.print("hello, world!");
+#endif /* USE_LIQUID_CRYSTAL */
+
+#ifdef USE_ACM1602NI
+  liquid_crystal_i2c.print("hello, world!");
+#endif /* USE_ACM1602NI */
+
+  delay(100);
+
+  for (int i=0 ; i< 100; i++) {
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+#ifdef USE_GROVE
+  lcd.setCursor(0, 1);
+#endif /* USE_GROVE */
+
+#ifdef USE_LIQUID_CRYSTAL
+  liquid_crystal.setCursor(0, 1);
+#endif /* USE_LIQUID_CRYSTAL */
+
+#ifdef USE_ACM1602NI
+  liquid_crystal_i2c.setCursor(0, 1);
+#endif /* USE_ACM1602NI */
+
+  // print the number of seconds since reset:
+
+#ifdef USE_GROVE
+  lcd.print(millis() / 1000);
+#endif /* USE_GROVE */
+
+#ifdef USE_LIQUID_CRYSTAL
+  liquid_crystal.print(millis() / 1000);
+#endif /* USE_LIQUID_CRYSTAL */
+
+#ifdef USE_ACM1602NI
+  liquid_crystal_i2c.print(millis() / 1000);
+#endif /* USE_ACM1602NI */
+  delay(100);
+  }
+
+  Serial.println("Hello World : end");
+  
+}
+
+void autoScroll(void) {
+  Serial.println("Auto Scroll : start");
+#ifdef USE_GROVE
+  lcd.setCursor(0, 0);
+#endif /* USE_GROVE */
+
+#ifdef USE_LIQUID_CRYSTAL
+  liquid_crystal.setCursor(0, 0);
+#endif /* USE_LIQUID_CRYSTAL */
+
+#ifdef USE_ACM1602NI
+  liquid_crystal_i2c.setCursor(0, 0);
+#endif /* USE_ACM1602NI */
+
+  for (int thisChar = 0; thisChar < 10; thisChar++) {
+#ifdef USE_GROVE
+    lcd.print(thisChar);
+#endif /* USE_GROVE */
+
+#ifdef USE_LIQUID_CRYSTAL
+    liquid_crystal.print(thisChar);
+#endif /* USE_LIQUID_CRYSTAL */
+
+#ifdef USE_ACM1602NI
+    liquid_crystal_i2c.print(thisChar);
+#endif /* USE_ACM1602NI */
+    delay(500);
+  }
+
+    // set the cursor to (16,1):
+#ifdef USE_GROVE
+  lcd.setCursor(16, 1);
+#endif /* USE_GROVE */
+
+#ifdef USE_LIQUID_CRYSTAL
+  liquid_crystal.setCursor(16, 1);
+#endif /* USE_LIQUID_CRYSTAL */
+
+#ifdef USE_ACM1602NI
+  liquid_crystal_i2c.setCursor(16, 1);
+#endif /* USE_ACM1602NI */
+  // set the display to automatically scroll:
+#ifdef USE_GROVE
+  lcd.setMode(AUTO_SCROLL);
+#endif /* USE_GROVE */
+
+#ifdef USE_LIQUID_CRYSTAL
+  liquid_crystal.setMode(AUTO_SCROLL);
+#endif /* USE_LIQUID_CRYSTAL */
+
+#ifdef USE_ACM1602NI
+  liquid_crystal_i2c.setMode(AUTO_SCROLL);
+#endif /* USE_ACM1602NI */
+
+  // print from 0 to 9:
+  for (int thisChar = 0; thisChar < 10; thisChar++) {
+#ifdef USE_GROVE
+    lcd.print(thisChar);
+#endif /* USE_GROVE */
+
+#ifdef USE_LIQUID_CRYSTAL
+    liquid_crystal.print(thisChar);
+#endif /* USE_LIQUID_CRYSTAL */
+
+#ifdef USE_ACM1602NI
+    liquid_crystal_i2c.print(thisChar);
+#endif /* USE_ACM1602NI */
+    delay(500);
+  }
+  // turn off automatic scrolling
+#ifdef USE_GROVE
+  lcd.setMode(NO_AUTO_SCROLL);
+#endif /* USE_GROVE */
+
+#ifdef USE_LIQUID_CRYSTAL
+  liquid_crystal.setMode(NO_AUTO_SCROLL);
+#endif /* USE_LIQUID_CRYSTAL */
+
+#ifdef USE_ACM1602NI
+  liquid_crystal_i2c.setMode(NO_AUTO_SCROLL);
+#endif /* USE_ACM1602NI */
+
+  Serial.println("Auto Scroll : end");
+
+}
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB
+  }
+  // set up the LCD's number of columns and rows:
+#ifdef USE_GROVE
+  lcd.begin(16, 2);
+#endif /* USE_GROVE */
+
+#ifdef USE_LIQUID_CRYSTAL
+  liquid_crystal.begin(16, 2);
+#endif /* USE_LIQUID_CRYSTAL */
+
+#ifdef USE_ACM1602NI
+  liquid_crystal_i2c.begin(16, 2);
+#endif /* USE_ACM1602NI */
+}
+
+void loop() {
+  clearDisplay();
+  helloWorld();
+  delay(100);
+  clearDisplay();
+  autoScroll();
+  clearDisplay();
+}
+
---
+++ src/UnifiedCharacterDisplay.cpp
@@ -0,0 +1,495 @@
+
+#include "UnifiedCharacterDisplay.h"
+
+
+#ifdef LCD_HAL_USE_GROVE
+UnifiedLCD::UnifiedLCD(rgb_lcd* lcd, UnifiedLcdType type, TwoWire wire):
+  _lcd(lcd),
+  _type(type),
+  _i2c(wire)
+{}
+#endif /* LCD_HAL_USE_GROVE  */
+
+#ifdef LCD_HAL_USE_ACM1602NI
+UnifiedLCD::UnifiedLCD(ACM1602NI* lcd, UnifiedLcdType type):
+  _liquid_crystal_i2c(lcd),
+  _type(type)
+{}
+#endif /* LCD_HAL_USE_ACM1602NI */
+
+#ifdef LCD_HAL_USE_LIQUID_CRYSTAL
+UnifiedLCD::UnifiedLCD(LiquidCrystal* lcd, UnifiedLcdType type):
+  _liquid_crystal(lcd),
+  _type(type)
+{}
+#endif /* LCD_HAL_USE_LIQUID_CRYSTAL */
+
+lcdFunctionReturnValue UnifiedLCD::begin(uint8_t cols, uint8_t rows, uint8_t charsize) {
+  switch(_type){
+#ifdef LCD_HAL_USE_GROVE
+    case GROVE_LCD_RGB_BACKLIGHT:
+      _lcd->begin(cols, rows, charsize, _i2c);
+      return FUNCTION_SUCCESS;
+#endif /* LCD_HAL_USE_GROVE */
+#ifdef LCD_HAL_USE_ACM1602NI
+    case LIQUID_CRYSTAL_I2C:
+      _liquid_crystal_i2c->begin(cols, rows);
+      return FUNCTION_SUCCESS;
+#endif /* LCD_HAL_USE_ACM1602NI */
+#ifdef LCD_HAL_USE_LIQUID_CRYSTAL
+    case LIQUID_CRYSTAL:
+      _liquid_crystal->begin(cols, rows, charsize);
+      return FUNCTION_SUCCESS;
+#endif /* LCD_HAL_USE_LIQUID_CRYSTAL */
+  }
+  return FUNCTION_UNSUPPORTED;
+}
+
+lcdFunctionReturnValue UnifiedLCD::clear(void) {
+  switch(_type){
+#ifdef LCD_HAL_USE_GROVE
+    case GROVE_LCD_RGB_BACKLIGHT:
+      _lcd->clear();
+      return FUNCTION_SUCCESS;
+#endif /* LCD_HAL_USE_GROVE */
+#ifdef LCD_HAL_USE_ACM1602NI
+    case LIQUID_CRYSTAL_I2C:
+      _liquid_crystal_i2c->clear();
+      return FUNCTION_SUCCESS;
+#endif /* LCD_HAL_USE_ACM1602NI */
+#ifdef LCD_HAL_USE_LIQUID_CRYSTAL
+    case LIQUID_CRYSTAL:
+      _liquid_crystal->clear();
+      return FUNCTION_SUCCESS;
+#endif /* LCD_HAL_USE_LIQUID_CRYSTAL */
+  }
+  return FUNCTION_UNSUPPORTED;
+}
+
+lcdFunctionReturnValue UnifiedLCD::home(void) {
+  switch(_type){
+#ifdef LCD_HAL_USE_GROVE
+    case GROVE_LCD_RGB_BACKLIGHT:
+      _lcd->home();
+      return FUNCTION_SUCCESS;
+#endif /* LCD_HAL_USE_GROVE */
+#ifdef LCD_HAL_USE_ACM1602NI
+    case LIQUID_CRYSTAL_I2C:
+      _liquid_crystal_i2c->home();
+      return FUNCTION_SUCCESS;
+#endif /* LCD_HAL_USE_ACM1602NI */
+#ifdef LCD_HAL_USE_LIQUID_CRYSTAL
+    case LIQUID_CRYSTAL:
+      _liquid_crystal->home();
+      return FUNCTION_SUCCESS;
+#endif /* LCD_HAL_USE_LIQUID_CRYSTAL */
+  }
+  return FUNCTION_UNSUPPORTED;
+}
+
+lcdFunctionReturnValue UnifiedLCD::setMode(UnifiedLcdMode mode) {
+  switch(mode) {
+    case NO_DISPLAY: return noDisplay();
+    case ON_DISPLAY: return display();
+    case NO_BLINK: return noBlink();
+    case BLINK: return blink();
+    case NO_CURSOR: return noCursor();
+    case CURSOR: return cursor();
+    case SCROLL_LEFT: return scrollDisplayLeft();
+    case SCROLL_RIGHT: return scrollDisplayRight();
+    case LEFT_TO_RIGHT: return leftToRight();
+    case RIGHT_TO_LEFT: return rightToLeft();
+    case AUTO_SCROLL: return autoscroll();
+    case NO_AUTO_SCROLL: return noAutoscroll();
+  }
+  return FUNCTION_UNSUPPORTED;
+}
+
+lcdFunctionReturnValue UnifiedLCD::noDisplay(void) {
+  switch(_type){
+#ifdef LCD_HAL_USE_GROVE
+    case GROVE_LCD_RGB_BACKLIGHT:
+      _lcd->noDisplay();
+      return FUNCTION_SUCCESS;
+#endif /* LCD_HAL_USE_GROVE */
+#ifdef LCD_HAL_USE_ACM1602NI
+    case LIQUID_CRYSTAL_I2C:
+      _liquid_crystal_i2c->noDisplay();
+      return FUNCTION_SUCCESS;
+#endif /* LCD_HAL_USE_ACM1602NI */
+#ifdef LCD_HAL_USE_LIQUID_CRYSTAL
+    case LIQUID_CRYSTAL:
+      _liquid_crystal->noDisplay();
+      return FUNCTION_SUCCESS;
+#endif /* LCD_HAL_USE_LIQUID_CRYSTAL */
+  }
+  return FUNCTION_UNSUPPORTED;
+}
+
+lcdFunctionReturnValue UnifiedLCD::display(void) {
+  switch(_type){
+#ifdef LCD_HAL_USE_GROVE
+    case GROVE_LCD_RGB_BACKLIGHT:
+      _lcd->display();
+      return FUNCTION_SUCCESS;
+#endif /* LCD_HAL_USE_GROVE */
+#ifdef LCD_HAL_USE_ACM1602NI
+    case LIQUID_CRYSTAL_I2C:
+      _liquid_crystal_i2c->display();
+      return FUNCTION_SUCCESS;
+#endif /* LCD_HAL_USE_ACM1602NI */
+#ifdef LCD_HAL_USE_LIQUID_CRYSTAL
+    case LIQUID_CRYSTAL:
+      _liquid_crystal->display();
+      return FUNCTION_SUCCESS;
+#endif /* LCD_HAL_USE_LIQUID_CRYSTAL */
+  }
+  return FUNCTION_UNSUPPORTED;
+}
+
+lcdFunctionReturnValue UnifiedLCD::noBlink(void) {
+  switch(_type){
+#ifdef LCD_HAL_USE_GROVE
+    case GROVE_LCD_RGB_BACKLIGHT:
+      _lcd->noBlink();
+      return FUNCTION_SUCCESS;
+#endif /* LCD_HAL_USE_GROVE */
+#ifdef LCD_HAL_USE_ACM1602NI
+    case LIQUID_CRYSTAL_I2C:
+      _liquid_crystal_i2c->noBlink();
+      return FUNCTION_SUCCESS;
+#endif /* LCD_HAL_USE_ACM1602NI */
+#ifdef LCD_HAL_USE_LIQUID_CRYSTAL
+    case LIQUID_CRYSTAL:
+      _liquid_crystal->noBlink();
+      return FUNCTION_SUCCESS;
+#endif /* LCD_HAL_USE_LIQUID_CRYSTAL */
+  }
+  return FUNCTION_UNSUPPORTED;
+}
+
+lcdFunctionReturnValue UnifiedLCD::blink(void) {
+  switch(_type){
+#ifdef LCD_HAL_USE_GROVE
+    case GROVE_LCD_RGB_BACKLIGHT:
+      _lcd->blink();
+      return FUNCTION_SUCCESS;
+#endif /* LCD_HAL_USE_GROVE */
+#ifdef LCD_HAL_USE_ACM1602NI
+    case LIQUID_CRYSTAL_I2C:
+      _liquid_crystal_i2c->blink();
+      return FUNCTION_SUCCESS;
+#endif /* LCD_HAL_USE_ACM1602NI */
+#ifdef LCD_HAL_USE_LIQUID_CRYSTAL
+    case LIQUID_CRYSTAL:
+      _liquid_crystal->blink();
+      return FUNCTION_SUCCESS;
+#endif /* LCD_HAL_USE_LIQUID_CRYSTAL */
+  }
+  return FUNCTION_UNSUPPORTED;
+}
+
+lcdFunctionReturnValue UnifiedLCD::noCursor(void) {
+  switch(_type){
+#ifdef LCD_HAL_USE_GROVE
+    case GROVE_LCD_RGB_BACKLIGHT:
+      _lcd->noCursor();
+      return FUNCTION_SUCCESS;
+#endif /* LCD_HAL_USE_GROVE */
+#ifdef LCD_HAL_USE_ACM1602NI
+    case LIQUID_CRYSTAL_I2C:
+      _liquid_crystal_i2c->noCursor();
+      return FUNCTION_SUCCESS;
+#endif /* LCD_HAL_USE_ACM1602NI */
+#ifdef LCD_HAL_USE_LIQUID_CRYSTAL
+    case LIQUID_CRYSTAL:
+      _liquid_crystal->noCursor();
+      return FUNCTION_SUCCESS;
+#endif /* LCD_HAL_USE_LIQUID_CRYSTAL */
+  }
+  return FUNCTION_UNSUPPORTED;
+}
+
+lcdFunctionReturnValue UnifiedLCD::cursor(void) {
+  switch(_type){
+#ifdef LCD_HAL_USE_GROVE
+    case GROVE_LCD_RGB_BACKLIGHT:
+      _lcd->cursor();
+      return FUNCTION_SUCCESS;
+#endif /* LCD_HAL_USE_GROVE */
+#ifdef LCD_HAL_USE_ACM1602NI
+    case LIQUID_CRYSTAL_I2C:
+      _liquid_crystal_i2c->cursor();
+      return FUNCTION_SUCCESS;
+#endif /* LCD_HAL_USE_ACM1602NI */
+#ifdef LCD_HAL_USE_LIQUID_CRYSTAL
+    case LIQUID_CRYSTAL:
+      _liquid_crystal->cursor();
+      return FUNCTION_SUCCESS;
+#endif /* LCD_HAL_USE_LIQUID_CRYSTAL */
+  }
+  return FUNCTION_UNSUPPORTED;
+}
+
+lcdFunctionReturnValue UnifiedLCD::scrollDisplayLeft(void) {
+  switch(_type){
+#ifdef LCD_HAL_USE_GROVE
+    case GROVE_LCD_RGB_BACKLIGHT:
+      _lcd->scrollDisplayLeft();
+      return FUNCTION_SUCCESS;
+#endif /* LCD_HAL_USE_GROVE */
+#ifdef LCD_HAL_USE_ACM1602NI
+    case LIQUID_CRYSTAL_I2C:
+      _liquid_crystal_i2c->scrollDisplayLeft();
+      return FUNCTION_SUCCESS;
+#endif /* LCD_HAL_USE_ACM1602NI */
+#ifdef LCD_HAL_USE_LIQUID_CRYSTAL
+    case LIQUID_CRYSTAL:
+      _liquid_crystal->scrollDisplayLeft();
+      return FUNCTION_SUCCESS;
+#endif /* LCD_HAL_USE_LIQUID_CRYSTAL */
+  }
+  return FUNCTION_UNSUPPORTED;
+}
+
+lcdFunctionReturnValue UnifiedLCD::scrollDisplayRight(void) {
+  switch(_type){
+#ifdef LCD_HAL_USE_GROVE
+    case GROVE_LCD_RGB_BACKLIGHT:
+      _lcd->scrollDisplayRight();
+      return FUNCTION_SUCCESS;
+#endif /* LCD_HAL_USE_GROVE */
+#ifdef LCD_HAL_USE_ACM1602NI
+    case LIQUID_CRYSTAL_I2C:
+      _liquid_crystal_i2c->scrollDisplayRight();
+      return FUNCTION_SUCCESS;
+#endif /* LCD_HAL_USE_ACM1602NI */
+#ifdef LCD_HAL_USE_LIQUID_CRYSTAL
+    case LIQUID_CRYSTAL:
+      _liquid_crystal->scrollDisplayRight();
+      return FUNCTION_SUCCESS;
+#endif /* LCD_HAL_USE_LIQUID_CRYSTAL */
+  }
+  return FUNCTION_UNSUPPORTED;
+}
+
+lcdFunctionReturnValue UnifiedLCD::leftToRight(void) {
+  switch(_type){
+#ifdef LCD_HAL_USE_GROVE
+    case GROVE_LCD_RGB_BACKLIGHT:
+      _lcd->leftToRight();
+      return FUNCTION_SUCCESS;
+#endif /* LCD_HAL_USE_GROVE */
+#ifdef LCD_HAL_USE_ACM1602NI
+    case LIQUID_CRYSTAL_I2C:
+      _liquid_crystal_i2c->leftToRight();
+      return FUNCTION_SUCCESS;
+#endif /* LCD_HAL_USE_ACM1602NI */
+#ifdef LCD_HAL_USE_LIQUID_CRYSTAL
+    case LIQUID_CRYSTAL:
+      _liquid_crystal->leftToRight();
+      return FUNCTION_SUCCESS;
+#endif /* LCD_HAL_USE_LIQUID_CRYSTAL */
+  }
+  return FUNCTION_UNSUPPORTED;
+}
+
+lcdFunctionReturnValue UnifiedLCD::rightToLeft(void) {
+  switch(_type){
+#ifdef LCD_HAL_USE_GROVE
+    case GROVE_LCD_RGB_BACKLIGHT:
+      _lcd->rightToLeft();
+      return FUNCTION_SUCCESS;
+#endif /* LCD_HAL_USE_GROVE */
+#ifdef LCD_HAL_USE_ACM1602NI
+    case LIQUID_CRYSTAL_I2C:
+      _liquid_crystal_i2c->rightToLeft();
+      return FUNCTION_SUCCESS;
+#endif /* LCD_HAL_USE_ACM1602NI */
+#ifdef LCD_HAL_USE_LIQUID_CRYSTAL
+    case LIQUID_CRYSTAL:
+      _liquid_crystal->rightToLeft();
+      return FUNCTION_SUCCESS;
+#endif /* LCD_HAL_USE_LIQUID_CRYSTAL */
+  }
+  return FUNCTION_UNSUPPORTED;
+}
+
+lcdFunctionReturnValue UnifiedLCD::autoscroll(void) {
+  switch(_type){
+#ifdef LCD_HAL_USE_GROVE
+    case GROVE_LCD_RGB_BACKLIGHT:
+      _lcd->autoscroll();
+      return FUNCTION_SUCCESS;
+#endif /* LCD_HAL_USE_GROVE */
+#ifdef LCD_HAL_USE_ACM1602NI
+    case LIQUID_CRYSTAL_I2C:
+      _liquid_crystal_i2c->autoscroll();
+      return FUNCTION_SUCCESS;
+#endif /* LCD_HAL_USE_ACM1602NI */
+#ifdef LCD_HAL_USE_LIQUID_CRYSTAL
+    case LIQUID_CRYSTAL:
+      _liquid_crystal->autoscroll();
+      return FUNCTION_SUCCESS;
+#endif /* LCD_HAL_USE_LIQUID_CRYSTAL */
+  }
+  return FUNCTION_UNSUPPORTED;
+}
+
+lcdFunctionReturnValue UnifiedLCD::noAutoscroll(void) {
+  switch(_type){
+#ifdef LCD_HAL_USE_GROVE
+    case GROVE_LCD_RGB_BACKLIGHT:
+      _lcd->noAutoscroll();
+      return FUNCTION_SUCCESS;
+#endif /* LCD_HAL_USE_GROVE */
+#ifdef LCD_HAL_USE_ACM1602NI
+    case LIQUID_CRYSTAL_I2C:
+      _liquid_crystal_i2c->noAutoscroll();
+      return FUNCTION_SUCCESS;
+#endif /* LCD_HAL_USE_ACM1602NI */
+#ifdef LCD_HAL_USE_LIQUID_CRYSTAL
+    case LIQUID_CRYSTAL:
+      _liquid_crystal->noAutoscroll();
+      return FUNCTION_SUCCESS;
+#endif /* LCD_HAL_USE_LIQUID_CRYSTAL */
+  }
+  return FUNCTION_UNSUPPORTED;
+}
+
+lcdFunctionReturnValue UnifiedLCD::createChar(uint8_t c, uint8_t data[]) {
+  switch(_type){
+#ifdef LCD_HAL_USE_GROVE
+    case GROVE_LCD_RGB_BACKLIGHT:
+      _lcd->createChar(c, data);
+      return FUNCTION_SUCCESS;
+#endif /* LCD_HAL_USE_GROVE */
+#ifdef LCD_HAL_USE_ACM1602NI
+    case LIQUID_CRYSTAL_I2C:
+      _liquid_crystal_i2c->createChar(c, data);
+      return FUNCTION_SUCCESS;
+#endif /* LCD_HAL_USE_ACM1602NI */
+#ifdef LCD_HAL_USE_LIQUID_CRYSTAL
+    case LIQUID_CRYSTAL:
+      _liquid_crystal->createChar(c, data);
+      return FUNCTION_SUCCESS;
+#endif /* LCD_HAL_USE_LIQUID_CRYSTAL */
+  }
+  return FUNCTION_UNSUPPORTED;
+}
+
+lcdFunctionReturnValue UnifiedLCD::createCharFromProgmem(uint8_t c, uint8_t * p) {
+  switch(_type){
+#ifdef LCD_HAL_USE_GROVE
+    case GROVE_LCD_RGB_BACKLIGHT:
+      _lcd->createChar(c, p);
+      return FUNCTION_SUCCESS;
+#endif /* LCD_HAL_USE_GROVE */
+#ifdef LCD_HAL_USE_ACM1602NI
+    case LIQUID_CRYSTAL_I2C:
+      _liquid_crystal_i2c->createChar(c, p);
+      return FUNCTION_SUCCESS;
+#endif /* LCD_HAL_USE_ACM1602NI */
+#ifdef LCD_HAL_USE_LIQUID_CRYSTAL
+    case LIQUID_CRYSTAL:
+      _liquid_crystal->createChar(c, p);
+      return FUNCTION_SUCCESS;
+#endif /* LCD_HAL_USE_LIQUID_CRYSTAL */
+  }
+  return FUNCTION_UNSUPPORTED;
+}
+
+lcdFunctionReturnValue UnifiedLCD::setCursor(uint8_t col, uint8_t row) {
+  switch(_type){
+#ifdef LCD_HAL_USE_GROVE
+    case GROVE_LCD_RGB_BACKLIGHT:
+      _lcd->setCursor(col, row);
+      return FUNCTION_SUCCESS;
+#endif /* LCD_HAL_USE_GROVE */
+#ifdef LCD_HAL_USE_ACM1602NI
+    case LIQUID_CRYSTAL_I2C:
+      _liquid_crystal_i2c->setCursor(col, row);
+      return FUNCTION_SUCCESS;
+#endif /* LCD_HAL_USE_ACM1602NI */
+#ifdef LCD_HAL_USE_LIQUID_CRYSTAL
+    case LIQUID_CRYSTAL:
+      _liquid_crystal->setCursor(col, row);
+      return FUNCTION_SUCCESS;
+#endif /* LCD_HAL_USE_LIQUID_CRYSTAL */
+  }
+  return FUNCTION_UNSUPPORTED;
+}
+
+size_t UnifiedLCD::write(uint8_t value) {
+  switch(_type){
+#ifdef LCD_HAL_USE_GROVE
+    case GROVE_LCD_RGB_BACKLIGHT:
+      return _lcd->write(value);
+#endif /* LCD_HAL_USE_GROVE */
+#ifdef LCD_HAL_USE_ACM1602NI
+    case LIQUID_CRYSTAL_I2C:
+      return _liquid_crystal_i2c->write(value);
+#endif /* LCD_HAL_USE_ACM1602NI */
+#ifdef LCD_HAL_USE_LIQUID_CRYSTAL
+    case LIQUID_CRYSTAL:
+      return _liquid_crystal->write(value);
+#endif /* LCD_HAL_USE_LIQUID_CRYSTAL */
+  }
+  return -1;
+}
+
+lcdFunctionReturnValue UnifiedLCD::command(uint8_t value) {
+  switch(_type){
+#ifdef LCD_HAL_USE_GROVE
+    case GROVE_LCD_RGB_BACKLIGHT:
+      _lcd->command(value);
+      return FUNCTION_SUCCESS;
+#endif /* LCD_HAL_USE_GROVE */
+#ifdef LCD_HAL_USE_ACM1602NI
+    case LIQUID_CRYSTAL_I2C:
+      _liquid_crystal_i2c->command(value);
+      return FUNCTION_SUCCESS;
+#endif /* LCD_HAL_USE_ACM1602NI */
+#ifdef LCD_HAL_USE_LIQUID_CRYSTAL
+    case LIQUID_CRYSTAL:
+      _liquid_crystal->command(value);
+      return FUNCTION_SUCCESS;
+#endif /* LCD_HAL_USE_LIQUID_CRYSTAL */
+  }
+  return FUNCTION_UNSUPPORTED;
+}
+
+lcdFunctionReturnValue UnifiedLCD::setBacklightRGB(unsigned char r, unsigned char g, unsigned char b) {
+  switch(_type){
+#ifdef LCD_HAL_USE_GROVE
+    case GROVE_LCD_RGB_BACKLIGHT:
+      _lcd->setRGB(r, g, b);
+      return FUNCTION_SUCCESS;
+#endif /* LCD_HAL_USE_GROVE */
+  }
+  return FUNCTION_UNSUPPORTED;
+}
+
+lcdFunctionReturnValue UnifiedLCD::blinkBacklight(void) {
+  switch(_type){
+#ifdef LCD_HAL_USE_GROVE
+    case GROVE_LCD_RGB_BACKLIGHT:
+      _lcd->blinkLED();
+      return FUNCTION_SUCCESS;
+#endif /* LCD_HAL_USE_GROVE */
+  }
+  return FUNCTION_UNSUPPORTED;
+}
+
+lcdFunctionReturnValue UnifiedLCD::noBlinkBacklight(void) {
+  switch(_type){
+#ifdef LCD_HAL_USE_GROVE
+    case GROVE_LCD_RGB_BACKLIGHT:
+      _lcd->noBlinkLED();
+      return FUNCTION_SUCCESS;
+#endif /* LCD_HAL_USE_GROVE */
+  }
+  return FUNCTION_UNSUPPORTED;
+}
+
+
